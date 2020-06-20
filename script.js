fc1057e7fa1224720c45ea646b998c2985766c7c
Hayden Linfoot <hayden.linfoot@gmail.com>
2020-06-20 14:13:35 -0500

    Updated with pulled master. Added JS code to display results. Next step is to link results with user inputed location

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ $(document).ready(function() {
         chosenCity=citySearch.val();
     });
 });
+
+$(function (){
+    
+    var $information = $('#information');
+       
+    $.ajax({
+        type: 'GET',
+        url: "https://data.medicare.gov/resource/yv7e-xc69.json",
+        success: function(information) {
+            $.each(information, function(i, selectedinfo) {
+                $information.append('<li>Hospital: ' + selectedinfo.hospital_name, 'Address ' + selectedinfo.city,' ' + selectedinfo.state, ' ' + selectedinfo.address, '</li>');
+            });
+        }
+    });
+   });    
