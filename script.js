6ec5e7f97def840d02d11a27bbc8a17483819bed
Hayden Linfoot <hayden.linfoot@gmail.com>
2020-06-20 10:56:36 -0500

    adding code. need to limit results.

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function (){
+    
+    var $information = $('#information');
+       
+    $.ajax({
+        type: 'GET',
+        url: "https://data.medicare.gov/resource/yv7e-xc69.json",
+        success: function(information) {
+            $.each(information, function(i, selectedinfo) {
+                $information.append('<li>Hospital: ' + selectedinfo.hospital_name, 'Address ' + selectedinfo.city,' ' + selectedinfo.state, ' ' + selectedinfo.address, '</li>');
+            });
+        }
+    });
+   });     
