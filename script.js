dd421033f39a810f2683dcee90ffed9cd1f1e0cc
Hayden Linfoot <hayden.linfoot@gmail.com>
2020-06-22 10:12:20 -0500

    Added variables that will eventually be submitted by user based on location.

--- script.js
+++ script.js
@@ -5,20 +5,24 @@ $(document).ready(function() {
     search.on('click', function() {
         event.preventDefault();
         chosenCity=citySearch.val();
+
+        
     });
 });
 
-$(function (){
+var api= 'https://data.medicare.gov/resource/yv7e-xc69.json?state='
+var state = 'TN'
+var city = 'CLARKSVILLE'
+
+var $information = $('#information')
+
+$.ajax({
+    type: 'GET',
+    url: api + state + '&city=' + city + '&condition=Emergency%20Department' ,
+    success: function(information) {
+        $.each(information, function(i, selectedinfo) {
+            $information.append('<li>Hospital: ' + selectedinfo.hospital_name, 'Address ' + selectedinfo.city,' ' + selectedinfo.state, ' ' + selectedinfo.address, '</li>');
+        });
+    }
+});
     
-    var $information = $('#information');
-       
-    $.ajax({
-        type: 'GET',
-        url: "https://data.medicare.gov/resource/yv7e-xc69.json",
-        success: function(information) {
-            $.each(information, function(i, selectedinfo) {
-                $information.append('<li>Hospital: ' + selectedinfo.hospital_name, 'Address ' + selectedinfo.city,' ' + selectedinfo.state, ' ' + selectedinfo.address, '</li>');
-            });
-        }
-    });
-   });    
