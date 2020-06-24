c62b58db704ea5e1d6f6be670382f9525e5c1c87
Hayden Linfoot <hayden.linfoot@gmail.com>
2020-06-23 20:23:25 -0500

    adding code that will connect states var with dropdown values.

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ $(document).ready(function() {
     var citySearch=$('#city-search');
     var chosenCity="";
     var $information = $('#information');
-    var state = 'TN';
+    var stateDropdown = $('#states')
+    var state = '';
 
     search.on('click', function() {
         event.preventDefault();
         chosenCity=citySearch.val();
+        state=stateDropdown.val();
         searchCityCurrent(chosenCity);
         $("#weather").show();
         $("#addresses").show();
